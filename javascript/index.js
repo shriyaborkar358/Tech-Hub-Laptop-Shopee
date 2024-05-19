f76817f68de5d73b2b004b9d783ebd3d443f25ac
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-05-19 16:15:30 +0530

    changes added in laptop card

--- javascript/index.js
+++ javascript/index.js
@@ -71,34 +71,67 @@ function imagesChange() {
    }
 }
 
-function inc() {
-   const quantityText = document.getElementById("quantity-text");
-   const showQuantity = document.getElementById("show-quantity");
-   const showTotal = document.getElementById("show-total");
+function inc1() {
+   const quantityText = document.getElementById("quantity-text-1");
+   const showQuantity = document.getElementById("show-quantity-1");
+   const showTotal = document.getElementById("show-total-1");
    let quantity = parseInt(quantityText.innerText);
    if (quantity < 5) {
-       quantityText.innerText = ++quantity;
+      quantityText.innerText = ++quantity;
    }
    else {
-       alert("you cann't add more than 5 items ")
+      alert("you cann't add more than 5 items ")
    }
    showQuantity.innerText = `Item : ${quantity} x 73,148`;
 
    showTotal.innerText = `Total: ${quantity * 73148}`;
 }
 
-function dec() {
-   const quantityText = document.getElementById("quantity-text");
-   const showQuantity = document.getElementById("show-quantity");
-   const showTotal = document.getElementById("show-total");
+function dec1() {
+   const quantityText = document.getElementById("quantity-text-1");
+   const showQuantity = document.getElementById("show-quantity-1");
+   const showTotal = document.getElementById("show-total-1");
    let quantity = parseInt(quantityText.innerText);
    if (quantity > 1) {
-       quantityText.innerText = --quantity;
+      quantityText.innerText = --quantity;
    }
    else {
-       alert("you cann't add less than 1 items")
+      alert("you cann't add less than 1 items")
    }
-   showQuantity.innerText = `Item : ${ quantity } X 73,148`;
+   showQuantity.innerText = `Item : ${quantity} X 73,148`;
    showTotal.innerText = `Total : ${quantity * 73148}`;
 
 }
+
+function inc2() {
+   const quantityText = document.getElementById("quantity-text-2");
+   const showQuantity = document.getElementById("show-quantity-2");
+   const showTotal = document.getElementById("show-total-2");
+   let quantity = parseInt(quantityText.innerText);
+   if (quantity < 5) {
+      quantityText.innerText = ++quantity;
+   }
+   else {
+      alert("you cann't add more than 5 items ")
+   }
+   showQuantity.innerText = `Item : ${quantity} x 83,780`;
+
+   showTotal.innerText = `Total: ${quantity * 83780}`;
+}
+
+function dec2() {
+   const quantityText = document.getElementById("quantity-text-2");
+   const showQuantity = document.getElementById("show-quantity-2");
+   const showTotal = document.getElementById("show-total-2");
+   let quantity = parseInt(quantityText.innerText);
+   if (quantity > 1) {
+      quantityText.innerText = --quantity;
+   }
+   else {
+      alert("you cann't add less than 1 items")
+   }
+   showQuantity.innerText = `Item : ${quantity} X 83,780`;
+   showTotal.innerText = `Total : ${quantity * 83780}`;
+
+}
+
