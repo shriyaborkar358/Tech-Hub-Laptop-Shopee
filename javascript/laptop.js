63b6dcd314b063478b7163a7289d762dfb52aa28
shriyaborkar358 <shriyaborkar71@gmail.com>
2024-05-20 13:26:44 +0530

    javascript added in laptop card

---
+++ javascript/laptop.js
@@ -0,0 +1,57 @@
+let toggleLike = true;
+
+function like1() {
+    const likeButton = document.getElementById("like1")
+    if (toggleLike) {
+        likeButton.src = "./../images/laptop-store-img/red-like.png";
+
+        toggleLike = false;
+    }
+    else {
+        likeButton.src = "./../images/laptop-store-img/blank-like.png";
+
+        toggleLike = true;
+    }
+}
+
+function like2() {
+    const likeButton = document.getElementById("like2")
+    if (toggleLike) {
+        likeButton.src = "./../images/laptop-store-img/red-like.png";
+
+        toggleLike = false;
+    }
+    else {
+        likeButton.src = "./../images/laptop-store-img/blank-like.png";
+
+        toggleLike = true;
+    }
+}
+
+function like3() {
+    const likeButton = document.getElementById("like3")
+    if (toggleLike) {
+        likeButton.src = "./../images/laptop-store-img/red-like.png";
+
+        toggleLike = false;
+    }
+    else {
+        likeButton.src = "./../images/laptop-store-img/blank-like.png";
+
+        toggleLike = true;
+    }
+}
+
+function like4() {
+    const likeButton = document.getElementById("like4")
+    if (toggleLike) {
+        likeButton.src = "./../images/laptop-store-img/red-like.png";
+
+        toggleLike = false;
+    }
+    else {
+        likeButton.src = "./../images/laptop-store-img/blank-like.png";
+
+        toggleLike = true;
+    }
+}
